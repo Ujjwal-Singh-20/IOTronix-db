bad80190c935da3a73cacbfd269381db90ab3afb
Ujjwal Singh <ujjusingh20@gmail.com>
2025-11-02 09:38:00 +0530

    deployed backend code

--- backend/main.py
+++ backend/main.py
@@ -109,6 +109,13 @@ async def get_teams():
     except Exception as e:
         return {"error": str(e)}
 
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
