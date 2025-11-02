b1ef72ebff55e4dc8c8755edf8f83c55b27ed2b4
Ujjwal Singh <ujjusingh20@gmail.com>
2025-11-02 19:30:16 +0530

    hosted backend

--- backend/main.py
+++ backend/main.py
@@ -112,3 +112,10 @@ async def get_teams():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     import os
+
+#     port = int(os.environ.get("PORT", 8080))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
